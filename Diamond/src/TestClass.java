f56c97ddee89f1f36509290d6ccebee794be6e7b
poojaamin450 <poojaamin450@gmail.com>
2022-01-26 23:10:55 +0530

    commited

---
+++ Diamond/src/TestClass.java
@@ -0,0 +1,34 @@
+
+public class TestClass {
+	public static void main(String[] args) {
+		interface First 
+		{  
+		    default void show() 
+		    { 
+		        System.out.println("Default First"); 
+		    } 
+		} 
+		interface Second 
+		{  
+		    default void show() 
+		    { 
+		        System.out.println("Default Second"); 
+		    } 
+		}  
+		class TestClass implements First, Second 
+		{ 
+		    public void show() 
+		    {  
+		        First.super.show(); 
+		        Second.super.show(); 
+		    } 
+		    public static void main(String args[]) 
+		    { 
+		        TestClass ob = new TestClass(); 
+		        ob.show(); 
+		    } 
+
+		}
+}
+
+}
